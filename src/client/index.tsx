fff14cda37b14819e06a93f7a3d35ca96fd60ea4
READTalk <redtalk.app@hotmail.com>
2026-03-19 10:30:09 +0700

    Update index.tsx

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -86,7 +86,7 @@ function App() {
 						"content",
 					) as HTMLInputElement;
 					const chatMessage: ChatMessage = {
-						id: nanoid(14),
+						id: nanoid(8),
 						content: content.value,
 						user: name,
 						role: "user",
